23a2657edfa6bd75d77fd78e637b2af569e333cb
Erik <santicasas667@gmail.com>
2023-11-08 08:38:15 -0500

    chakra-UI

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,11 @@
-import "../styles/Card.css";
 const Card = () => {
 
+  const pikachu = {
+    name: "pikachu",
+    image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+    type: "electric"
+  }
+// importa los componentes necesarios para mostrar una carta con pikachu
   return (
     <>
 
