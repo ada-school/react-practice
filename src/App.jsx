9cb749af11968655f443f13b49885a13ca1c20bf
Erik <santicasas667@gmail.com>
2023-11-08 08:20:31 -0500

    templete

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,10 @@
-import { Suspense, lazy } from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import ListPokemons from "./pages/ListPokemons";
-import Header from "./components/Header";
-import Error from "./components/Error";
-import Detail from "./pages/Detail";
-import About from "./pages/About";
+import Card from "./components/Card";
+
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Header />
-
-        <Routes>
-          <Route path="/" element={<ListPokemons />} />
-          <Route path="/about" element={<About />} />
-
-          <Route path="/pokemon/:idPokemon" element={<Detail />} />
-          <Route path="*" element={<Error />} />
-        </Routes>
-      </BrowserRouter>
+      <Card/>
     </>
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,25 +1,11 @@
 import React from "react";
 import "../styles/Card.css";
-import { Link } from "react-router-dom";
-const Card = ({ data }) => {
+const Card = () => {
 
   return (
     <>
 
-    <Link to={"pokemon/"+data.id}> 
-        <div className="card-pokemon">
-          <div className="card-img">
-            <img src={data.image} alt={`Pokemon ${data.name}`} />
-          </div>
-          <div className="card-info">
-            <span className="pokemon-id">N° {data.id}</span>
-            <h3>{data.name}</h3>
-            <div className="card-types">
-              <span className={data.type}>{data.type}</span>
-            </div>
-          </div>
-        </div>
-    </Link>
+   
     </>
   );
 };
