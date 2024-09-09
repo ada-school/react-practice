8adb3d3fd507bbe6405738bb0024daff2d7009e6
ErikCasas <santicasas667@gmail.com>
2024-09-09 14:04:07 -0500

    remove components

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,12 @@
 import React from "react";
 import "./App.css";
 
-import Decrement from "./components/Decrement";
-import Increment from "./components/Increment";
-import Counter from "./components/counter";
-import ContextProvider from "./Context/contextProvider";
-
 function App() {
-  return (
-    <>
-      <ContextProvider>
-        <Counter />
-        <Increment />
-        <Decrement />
-      </ContextProvider>
-    </>
-  );
+	return (
+		<>
+			<h1>Ada school!</h1>
+		</>
+	);
 }
 
 export default App;
