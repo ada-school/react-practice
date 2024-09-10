4e7045ebea9a46a4764ba7ab1eca9484a4e79f07
ErikCasas <santicasas667@gmail.com>
2024-09-09 22:13:07 -0500

    classroom practice template

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
-import React from "react";
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+	const [numbers, setNumbers] = useState([10, 20, 5, 30, 50, 15]);
+	const [count, setCount] = useState(0);
+
+	const maxNumber = () => {
+		console.log("Calculando el número máximo...");
+		return Math.max(...numbers);
+	};
+
+	const addRandomNumber = () => {
+		const randomNumber = Math.floor(Math.random() * 100) + 1;
+		setNumbers((prevNumbers) => [...prevNumbers, randomNumber]);
+	};
+
 	return (
-		<>
-			<h1>Ada school!</h1>
-		</>
+		<div>
+			<button onClick={() => setCount((prev) => prev + 1)}>
+				contador en: {count}
+			</button>
+			<hr />
+			<p>Lista de números: {numbers.join(", ")}</p>
+			<p>Número máximo: {maxNumber()}</p>
+			<button onClick={addRandomNumber}>Agregar número aleatorio</button>
+		</div>
 	);
 }
 
---
+++ src/README.md
@@ -0,0 +1,6 @@
+# useRef y useMemo
+
+1. En el componente `App.jsx` crea un contador de la cantidad de veces que se ha renderizado la aplicación haciendo uso del hook useRef
+2. Ajusta la función `maxNumber` para que sea una implementación del hook `useMemo` y así solo busque el numero mas grande cuando sea necesario
+3. Ajusta el HTML del componente para que enseñe la cantidad de renderizados que ha tenido
+4. Con ayuda de tus compañeros realiza comentarios sobre el código explicando el paso a paso de que está pasando
